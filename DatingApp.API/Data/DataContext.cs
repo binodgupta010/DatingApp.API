cdaf83423530bfd81dcc94c56a84d9a472a2a440
Binod Kumar Gupta <binod.gupta@tavant.com>
2019-03-01 18:40:09 +0530

    added db file

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,7 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DatingApp.API.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> option)  : base (option)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
         
     }
 }
